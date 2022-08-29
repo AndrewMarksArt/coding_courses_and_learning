5f1270ea62cc7319a94babeade1bc82d310bc69c
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-29 13:07:26 -0700

    created new branch for ch1 and set up instance exersice file

---
+++ ExerciseFiles/Ch 1/instance_start.py
@@ -0,0 +1,21 @@
+# Using instance methods and attributes
+
+
+class Book:
+    # the "init" function is called when the instance is
+    # created and ready to be initialized
+    def __init__(self, title):
+        self.title = title
+        # TODO: add properties
+
+    # TODO: create instance methods
+
+
+# TODO: create an instances of the class
+b1 = Book("Treasure Island")
+b2 = Book("War and Peace")
+
+# TODO: print the cprice of book1
+
+
+# TODO: try setting the discount
