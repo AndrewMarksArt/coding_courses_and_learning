96f29c6238f4a5e4f8087437d948c8f21a98cf7e
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-29 12:13:43 -0700

    added folders for exercises and ch's

---
+++ ExerciseFiles/Ch 1/definition_start.py
@@ -0,0 +1,13 @@
+# TODO: create a basic class
+class Book:
+    def __init__(self, title) -> None:
+        self.title = title
+
+
+# TODO: create an instances of the class
+b1 = Book("Treasure Island")
+b2 = Book("War and Peace")
+
+# TODO: print the class and property
+print(b1.title)
+print(b2.title)
