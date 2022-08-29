39d8a487650f070c253263d624507dcfbec9e292
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-29 13:30:01 -0700

    finished checktype exercises

--- ExerciseFiles/Ch 1/typecheck_start.py
+++ ExerciseFiles/Ch 1/typecheck_start.py
@@ -1,7 +1,5 @@
 # Checking class types and instances
 
-from unicodedata import name
-
 
 class Book:
     def __init__(self, title) -> None:
@@ -18,3 +16,17 @@ b1 = Book("The Catcher In The Rye")
 b2 = Book("The Grapes of Wrath")
 n1 = Newspaper("The Washington Post")
 n2 = Newspaper("The New York Times")
+
+# TODO: use type() to inspect the object
+print(type(b1))
+print(type(n1))
+
+# TODO: compare two types together
+print(type(b1) == type(b2))
+print(type(b1) == type(n2))
+
+# TODO: use isinstance to compare a specific instance to a known type
+print(isinstance(b1, Book))
+print(isinstance(n1, Newspaper))
+print(isinstance(n1, Book))
+print(isinstance(n1, object))
