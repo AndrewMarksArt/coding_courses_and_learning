389785c68b19727dafc1a8b0de1de04455a50b7b
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-30 15:49:28 -0700

    added frozen=True to dataclass decorator to see how it makes the dataclass immutable

--- ExerciseFiles/Ch_4/immutable.py
+++ ExerciseFiles/Ch_4/immutable.py
@@ -3,16 +3,21 @@
 from dataclasses import dataclass
 
 
-@dataclass
+# TODO: the Frozen parameter makes the class immutable
+@dataclass(frozen=True)
 class ImmutableClass:
     value1: str = "Value 1"
     value2: int = 0
+
+    def somefunc(self, newValue):
+        self.value2 = newValue
 
 
 obj = ImmutableClass()
 print(obj.value1)
 
 # TODO: attempting to change the value of an immutable class throws an exception
-
+# obj.value1 = "new value"
 
 # TODO: even functions within the class can't change anything
+obj.somefunc(10)
