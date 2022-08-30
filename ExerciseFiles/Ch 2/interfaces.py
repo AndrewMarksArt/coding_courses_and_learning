dc7575f8795986d37f437fccb812bed42fb56e2c
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-30 09:28:52 -0700

    added abstract base class and abstract method and used them with a child class that uses multiple inheritance to implement an interface

--- ExerciseFiles/Ch 2/interfaces.py
+++ ExerciseFiles/Ch 2/interfaces.py
@@ -2,6 +2,12 @@
 from abc import ABC, abstractmethod
 
 
+class JSONify(ABC):
+    @abstractmethod
+    def toJSON(slef):
+        pass
+
+
 class GraphicShape(ABC):
     def __init__(self) -> None:
         super().__init__()
@@ -11,13 +17,17 @@ class GraphicShape(ABC):
         pass
 
 
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius) -> None:
         self.radius = radius
 
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
 
+    def toJSON(self):
+        return f"{{\"circle\" : {str(self.calcArea())}}}"
+
 
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
