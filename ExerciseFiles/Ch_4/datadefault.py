bb7135119cf5d0aa47a4275fcab71fe0fb55009f
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-30 15:39:34 -0700

    added datadefault.py to look at how to set default values for dataclasses

---
+++ ExerciseFiles/Ch_4/datadefault.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+import random
+
+def price_func():
+    return float(random.randrange(20, 40))
+
+
+@dataclass
+class Book:
+    # you can define default values when attributes are declared
+    # non default attributies must come before attributes with defaults
+    title: str = "No Title"
+    author: str = "No Author"
+    pages: int = 0
+    price: float = field(default_factory=price_func)
+
+
+b1 = Book("War and Peace", "Leo Tolstoy", 1225)
+b2 = Book("The Catcher and the Rye", "JD Salinger", 234)
+
+print(b1)
+print(b2)
