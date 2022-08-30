22bd71fe2a331aa66850deeaaff439ea3721af55
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-30 14:10:34 -0700

    modified the __call__method and showed how it can be used to change an objects attrs like a method

--- ExerciseFiles/Ch_3/callable_objs.py
+++ ExerciseFiles/Ch_3/callable_objs.py
@@ -10,9 +10,18 @@ class Book:
 
 
     # TODO: the __call__ method can be used to call the object like a function
+    # good for object that change frequently or have attributes that change together,
+    # more compact and easy to read
+    def __call__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher and the Rye", "JD Salinger", 29.95)
 
 # TODO: call the object as if it were a function
+print(b1)
+b1("Anna Karenina", "Leo Tolstoy", 49.95)
+print(b1)
