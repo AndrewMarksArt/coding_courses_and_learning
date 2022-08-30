caa049c25d4eb6e1acb4839cdfb9328c568423ac
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-30 14:27:16 -0700

    finished dataclasses intro, saw how we don't need to use __init__ or self with dataclasses

--- ExerciseFiles/Ch_4/dataclass.py
+++ ExerciseFiles/Ch_4/dataclass.py
@@ -1,20 +1,34 @@
+# starting with python 3.7 -- dataclasses decorator for classes that mostly just hold data
+# Using data classes to represent data objects
+from dataclasses import dataclass
+
+@dataclass
 class Book:
-    def __init__(self, title, author, pages, price):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.price = price
+    title: str
+    author: str
+    pages: int
+    price: float
+
+    def bookinfo(self):
+        return f"{self.title}, by {self.author}"
 
 
 # create some instances
 b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
 b2 = Book("The Catcher and the Rye", "JD Salinger", 234, 29.95)
+b3 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
 
 # access fields
 print(b1.title)
 print(b2.author)
 
 # TODO: print the book itself - dataclasses implement __repr__
-
+print(b1)
 
 # TODO: comparing two dataclasses - they implement __eq__
+print(b1 == b3)
+
+# TODO: change some attributes
+b1.title = "Anna Karenina"
+b1.pages = 864
+print(b1.bookinfo())
