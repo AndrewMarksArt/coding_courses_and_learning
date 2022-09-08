9669e547d2d04fdd162c3be9d6dc643845a938b5
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-08 16:42:30 -0700

    Looked at HTTP/HTTPs modules and creating an express server

--- Learning_Javascript_Full_Stack/server.js
+++ Learning_Javascript_Full_Stack/server.js
@@ -1,4 +1,22 @@
-import config, { nodeEvn, logStars } from './config';
+import config from './config';
+import apiRouter from './api';
 
-console.log(config, nodeEvn)
-logStars('function')
+import express from 'express';
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello Express');
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+/* server.get('/about.html', (req, res) => {
+    fs.readFile('./about.html', (err, data) => {
+        res.send(data.toString());
+    });
+}); */
+
+server.listen(config.port, () => {
+    console.log('Express Listening on port ', config.port);
+});
